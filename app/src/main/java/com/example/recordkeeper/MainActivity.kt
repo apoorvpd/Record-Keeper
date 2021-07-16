fc3d812cb67c1f1a6f775c936e9bc3df26d2495c
apoorv127 <prasada@uwm.edu>
2021-07-16 18:12:29 -0500

    ViewBinding - The current best way to find Views

--- app/src/main/java/com/example/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.recordkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.recordkeeper.databinding.ActivityMainBinding
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textViewExample.text = "We can now use View Binding. Hooray!"
+
+
     }
 }
