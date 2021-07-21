727c719be0b073a6c3f0adb31501765dcfe8ddf4
apoorv127 <prasada@uwm.edu>
2021-07-21 08:02:18 -0500

    Setting up the click listeners, and 'context' in Fragments

--- app/src/main/java/com/example/recordkeeper/RunningFragment.kt
+++ app/src/main/java/com/example/recordkeeper/RunningFragment.kt
@@ -1,5 +1,6 @@
 package com.example.recordkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -20,6 +21,26 @@ class RunningFragment : Fragment(){
         binding = FragmentRunningBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.container5km.setOnClickListener { launchRunningRecordScreen() }
+        binding.container10km.setOnClickListener { launchRunningRecordScreen() }
+        binding.containerHalfMarathon.setOnClickListener { launchRunningRecordScreen() }
+        binding.containerFullMarathon.setOnClickListener { launchRunningRecordScreen() }
+    }
+
+    private fun launchRunningRecordScreen() {
+        // context -  same as the MainActivity
+        // In Activity class, we can use 'this' for Intent context argument.
+        // In a Fragment, we can't use 'this', but we can utilize 'context' to get that access.
+        val intent = Intent(context, EditRunningRecordActivity::class.java)
+        startActivity(intent)
     }
 
 }
